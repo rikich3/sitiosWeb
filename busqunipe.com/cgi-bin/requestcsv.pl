26c734bb951f44322ae479ca386fa0d59b4d5118
rikich3 <rchambillap@unsa.edu.pe>
2024-01-09 07:59:23 -0500

    Completado el laboratorio sobre las busquedas de universidades

--- busqunipe.com/cgi-bin/requestcsv.pl
+++ busqunipe.com/cgi-bin/requestcsv.pl
@@ -2,32 +2,47 @@
 use strict;
 use warnings;
 use CGI;
-use LWP::Simple;  # Make sure to have LWP::Simple installed
+use LWP::Simple;
 
-# Create a CGI object
 my $cgi = CGI->new;
 my $url = "https://www.datosabiertos.gob.pe/sites/default/files/Programas de Universidades.csv";
-# Print Content-Type header
 print $cgi->header('text/html');
 
-# Retrieve the file URL from the form submission
-my $file_url = $cgi->param('nomUni');
-my $file_url = $cgi->param('periodo');
-my $file_url = $cgi->param('depar');
-my $file_url = $cgi->param('programa');
+my $kind = $cgi->param('category');
+my $keyword = $cgi->param('searchTerm');
 my @lines = ();
 
-# Validate the file URL (add more validation if needed)
 if ($file_url =~ /^https?:\/\/\S+$/) {
-    # Use LWP::Simple to fetch the file content
     my $file_content = get($file_url);
-
     if (defined $file_content) {
-        # Split the content into lines
         @lines = split /\n/, $file_content;
     } else {
         print "Failed to fetch file. Check the URL and try again.";
     }
 } else {
     print "Invalid file URL. Please enter a valid URL.";
+}
+if(!($kind eq "") && !($keyword eq "")){
+  #open(IN,"../data/data.txt") or die "<h1>ERROR: open file</h1>\n";
+  #esta comentado esta ultima linea debido a que el archivo se esta extrayendo de la misma web
+  for my $line (@lines){
+    my %dict = matchLine($line);
+    my $value = $dict{$kind};
+    if(defined($value) && $value =~ /.*$keyword.*/){
+      print "<h1>Encontrado: $line</h1>\n";
+      print "<p>".$value."</p>";
+      $flag = 1;
+      next; #continue the loop
+    }
+  }
+}
+sub matchLine{
+  my %dict = ();
+  my $line = $_[0];
+  my @split_array = split (/\|/, $line);
+  $dict{"nombre"} = $split_array[1];
+  $dict{"licenciamiento"} = $split_array[3];
+  $dict{"departamento"} = $split_array[7];
+  $dict{"programa"} = $split_array[18];
+  return %dict;
 }
